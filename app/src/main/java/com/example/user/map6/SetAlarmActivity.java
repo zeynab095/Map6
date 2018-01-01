770a8633e4871c771a045c188f07dbbedb645b33
Sevinj Jafarli <sjafarli@outlook.com>
2018-01-01 13:35:36 +0400

    PickUp-->SetDirection-->Specify Meter and Destination, Save it; Click Saved Destination-->Load to Destination page

--- app/src/main/java/com/example/user/map6/SetAlarmActivity.java
+++ app/src/main/java/com/example/user/map6/SetAlarmActivity.java
@@ -19,12 +19,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class SetAlarmActivity extends AppCompatActivity implements
         SharedPreferences.OnSharedPreferenceChangeListener {
-
+    TextView dest;
+    Spinner spinner;
     float desLat;
     float desLong;
     private static final String TAG = SetAlarmActivity.class.getSimpleName();
@@ -89,12 +93,46 @@ public class SetAlarmActivity extends AppCompatActivity implements
         mRemoveLocationUpdatesButton = (Button) findViewById(R.id.remove_location_updates_button);
         mRequestLocationUpdatesButton.setEnabled(true);
         mRemoveLocationUpdatesButton.setEnabled(false);
+
+
+        dest=(TextView) findViewById(R.id.dest); //destination name to save to shared preferneces
+        spinner = (Spinner) findViewById(R.id.list);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.meters, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        Bundle b1= getIntent().getExtras();
+        if(b1!=null) {
+            String name = b1.getString("name");
+
+            SharedPreferences sh1 = getSharedPreferences("MyOwnShared", MODE_PRIVATE);
+            String s1 = sh1.getString(name, "");
+            String[] arr = s1.split(",");
+            dest.setText(arr[0]);
+//            spinner.setSelection(Integer.parseInt(arr[1]));
+        }
+
         // Check that the user hasn't revoked permissions by going to Settings.
         if (Utils.requestingLocationUpdates(this)) {
             if (!checkPermissions()) {
                 requestPermissions();
             }
         }
+    }
+    public void saveData(View view) {
+        String s[]={dest.getText().toString(),String.valueOf(spinner.getSelectedItemPosition())};
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length; i++) {
+            sb.append(s[i]).append(",");
+        }
+        SharedPreferences sh = getSharedPreferences("MyOwnShared",MODE_PRIVATE);
+        SharedPreferences.Editor myEdit =sh.edit();
+        myEdit.putString(dest.getText().toString(),sb.toString());
+        myEdit.commit();
     }
 
     @Override
